b731479925e9d3e73b0d451e7d9dd66798ce9e4b
rroyCFD <roy.rajib@live.com>
2018-05-29 16:04:42 -0600

    Initial commit for piso solver for periodic channels

---
+++ pisoChannelFoamMod.C
@@ -0,0 +1,113 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+Application
+    pisoChannelFoamMod
+
+Description
+    Transient solver for incompressible flow in a periodic channel.
+    Turbulence modelling is generic, i.e. laminar, RAS or LES may be selected.
+
+    Algorithom is implemented followed by the outline in book:
+        The Finite Volume Method in Computational Fluid Dynamics
+        Section 15.7 (SIMPLE family of algorithoms)
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulenceModel.H"
+#include "orthogonalSnGrad.H"
+
+#include "IFstream.H"
+#include "OFstream.H"
+#include "IOmanip.H" // for input/ouput format control
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "readTransportProperties.H"
+    #include "createFields.H"
+    #include "initContinuityErrs.H"
+    //#include "readTimeControls.H" // for fixed courant number
+    #include "createGradP.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    //Rhie Chow interpolation stuff
+    const surfaceVectorField ed = mesh.delta()()/mag(mesh.delta()());
+    Foam::fv::orthogonalSnGrad<scalar> faceGradient(mesh);
+
+    //surfaceVectorField gradp_avg_f = linearInterpolate(fvc::grad(p));
+    surfaceVectorField gradpDiff_f
+        =
+        -(linearInterpolate(fvc::grad(p)) & ed)*ed
+        + (faceGradient.snGrad(p))*ed;
+
+    // adding gradU for open-boundary pressure estimation
+    volTensorField gradU("gradU", fvc::grad(U));
+
+    // run-time coverge and advance loop
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        #include "readPISOControls.H"
+        #include "CourantNo.H"
+        //#include "setDeltaT.H" // for fixed courant number, variable timeStep
+
+        // store old values for temporal discretization,
+        // & temporal correction to phi in Rhie-Chow flux calculation.
+        U.storeOldTime();
+        phi.storeOldTime();
+
+        // --- PISO loop
+        for (int corr=1; corr<=nCorr; corr++)
+        {
+            #include "UEqn.H"
+            #include "ppEqn.H"
+
+            // PRIME loop
+            for (int nPrime=1; nPrime <= nPrimeIterations; nPrime++)
+            {
+                #include "PRIME.H"
+
+                // calculate gradP and update velocity field (at the end of convergence)
+                if(nPrime == nPrimeIterations && corr == nCorr)
+                {
+                    #include "calculateGradP.H"
+                }
+            }
+            // end of PRIME loop
+
+        }// end of corrector loop
+
+        #include "continuityErrs.H"
+
+        turbulence->correct();
+
+        gradU = fvc::grad(U);
+
+        runTime.write();
+        #include "writeGradP.H"
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    } // end of runTime loop
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
